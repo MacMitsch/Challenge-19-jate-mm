1316e3eb5bf689f915ce580afa3a5316b427850f
macm <mac.mitsch@comcast.net>
2022-09-11 10:31:37 -0500

    updated webpack.config and src-sw

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -1,47 +1,79 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "production",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name]bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
+    // Configures workbox plugins 
     plugins: [
       new HtmlWebpackPlugin({
-        template:"./index.html",
-        title: "J.A.T.E.",
+        template: "./index.html",
+        title: "webpack Plugin",
       }),
-
-      new InjectManifest ({
+// Injects custom service worker from src-sw
+      new InjectManifest({
         swSrc: "./src-sw.js",
-        swDest: "src-sw.js"
+        swDest: "service-worker.js",
       }),
-      
+// Creates a manifest file 
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
         name: "Just Another Text Editor",
         short_name: "J.A.T.E.",
-        description: "A Text Editor using IndexedDB with online and offline Capabilites",
-        background_color: 
-      })
-      
+        description:
+          "A Text Editor using IndexedDB with online and offline Capabilites",
+        background_color: "#000000",
+        theme_color: "#31a9e1",
+        start_url: "/",
+        publicPath: "/",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 144, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
     ],
 
+    // Adding babel and CSS loaders to webpack
     module: {
       rules: [
-        
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          user: "asset/resource",
+        },
+        {
+          test: /\.css$/i,
+          use:[ "style-loader","css-loader"],
+        },
+        {
+          test: /\.n?js$/,
+          exclude: /node_modules/,
+          user: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/present-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        },
       ],
     },
   };
